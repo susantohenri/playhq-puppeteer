eb0256f327807fdca91dff8cdf5925403145a7e4
Henri Susanto <henry.dinus@gmail.com>
2025-02-26 11:33:56 +0700

    aws: read xls

--- index.mjs
+++ index.mjs
@@ -1,8 +1,26 @@
+import path from 'path';
+import xlsxPopulate from 'xlsx-populate';
+
 export const handler = async (event) => {
-  // TODO implement
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!'),
+    body: JSON.stringify(await readPlayerNames()),
   };
   return response;
 };
+
+async function readPlayerNames() {
+  const currentDir = path.dirname(new URL(import.meta.url).pathname);
+  const filePath = path.join(currentDir, 'template.xlsx');
+  const workbook = await xlsxPopulate.fromFileAsync(filePath);
+  const sheet = workbook.sheet(`Template`);
+  let row = 2;
+  const names = [];
+  while (true) {
+      const name = sheet.cell(`B${row}`).value();
+      row++;
+      if (undefined === name) break;
+      else names.push(name.replace(String.fromCharCode(160), String.fromCharCode(32)))
+  }
+  return names;
+}
